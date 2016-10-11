dd68acf8aadaab3fe46f25b49f3a45fedff11e28
Chris Craig <Codeman655@gmail.com>
2016-10-11 12:40:06 -0400

    still working on it

--- facehugger.rb
+++ facehugger.rb
@@ -1,15 +1,19 @@
 #!/usr/bin/env ruby
 require 'yaml'
+require 'rubygems/package'
 require 'thor'
 require 'pathname'
+require 'fileutils'
 
 #default config
-config = Pathname.new(File.join(ENV["HOME"],".config", "facehugger", "facehugger.yml"))
+Configdir = Pathname.new(File.join(ENV["HOME"],".config", "facehugger"))
+Config = Pathname.new(File.join(Configdir, "facehugger.yml"))
+
 def read_config
     #puts join("templates", "example.html")
-    fp = open(config,'r')
+    fp = open(Config,'r')
     files = YAML.load(fp.read)
-    #puts files
+    puts files
 
     #files.each_pair { |key,val|
     #    puts key
@@ -31,33 +35,45 @@ class Facehugger < Thor
         puts "injecting the following files to the targeted host"
     end
 
+    #Credit: http://weblog.jamisbuck.org/2015/7/23/tar-gz-in-ruby.html
     desc "collect", "tar all config files into a collection"
     option :dest
     def collect(opt)
         #Read config
         files = self.read_config
 
+        #Check if files exist
+        files.each { |f|
+            if File.exists? f then
+                puts "#{f} does not exist."
+                exit
+            end
+        }
+
         #Check if tar( or tar libraries) are available
         #tar files 
-        puts "tar -czf config_egg.tar.gz #{files}"
+        puts "tar -czf #{Configdir}/config_egg.tar.gz #{files}"
     end
 
-    desc "alien", "prints a little image"
+    desc "alien", "prints an alien"
     def alien()
-        puts File.read('/Users/chricrai/Documents/Code/facehugger/alien.txt')
+        puts File.read('alien.txt')
     end
 
     desc "init", "create a blank config file for facehugger"
     def init()
-        if File.exists? config then
-            puts "config file already exists at #{config}"
+        unless File.exists? Configdir 
+            FileUtils::mkdir_p Configdir
+        end
+
+        if File.exists? Config then
+            puts "config file already exists at #{Config}"
         else
-            puts "Creating config file in #{config}"
-            fp = File.open(config,'w')
-            bash_sample = File.join(ENV[HOME], ".bashrc")
-            fp.write('---'\
-                     'bash:'\
-                     '  configfile: "#{bash_sample}"')
+            puts "Creating config file in #{Config}"
+            fp = File.open(Config,'w')
+            bash_sample = File.join(ENV["HOME"], ".bashrc")
+            output={"bash"=>{"configfile"=>"#{bash_sample}"}}
+            fp.puts(YAML.dump(output))
             fp.close
         end
     end
