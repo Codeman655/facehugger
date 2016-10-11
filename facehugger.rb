a51a365d39a8403ade3729861c7e9805965e7d6b
Chris Craig <Codeman655@gmail.com>
2016-10-11 14:17:23 -0400

    Collect works but is limited to just Unix/Mac

--- facehugger.rb
+++ facehugger.rb
@@ -12,21 +12,21 @@ Config = Pathname.new(File.join(Configdir, "facehugger.yml"))
 def read_config
     #puts join("templates", "example.html")
     fp = open(Config,'r')
-    files = YAML.load(fp.read)
-    puts files
+    YAML.load(fp.read)
 
-    #files.each_pair { |key,val|
-    #    puts key
-    #    puts val
-    #    if File.exists?(val["configfile"]) then
-    #        puts "#{val} exists"
-    #    end
-    #puts f["configfile"]
-    #puts f[1]['configfile']
-    #if File.exists? f[:configfile] then
-    #puts f[:configfile]
-    #end
-    #}
+	#files.each_pair { |key,val|
+		#puts key
+		#puts val
+		#if File.exists?(val["configfile"]) then
+			#puts "#{val} exists"
+		#end
+#
+		#puts f["configfile"]
+		#puts f[1]['configfile']
+		#if File.exists? f[:configfile] then
+			#puts f[:configfile]
+		#end
+	#}
 end
 
 class Facehugger < Thor
@@ -37,23 +37,62 @@ class Facehugger < Thor
 
     #Credit: http://weblog.jamisbuck.org/2015/7/23/tar-gz-in-ruby.html
     desc "collect", "tar all config files into a collection"
-    option :dest
-    def collect(opt)
-        #Read config
-        files = self.read_config
+    option :dest, :required=>false
+	def collect()
+		#Read config
+		files = read_config
 
-        #Check if files exist
-        files.each { |f|
-            if File.exists? f then
-                puts "#{f} does not exist."
-                exit
-            end
-        }
+		#Check if files exist
+		list = []
+		files.each_pair { |f,c|
+			c.each_pair { |key, val|
+				case key
+				when 'configfile'
+					unless File.exists? val then
+						puts "#{val} does not exist."
+					else
+						list.push val
+					end
+				when 'configdir'
+					unless File.exists? val then
+						puts "#{val} does not exist."
+					else
+						list.push val
+					end
+				when 'command'
+					puts "command: " + val
+				else
+					'invalid key'
+				end
+			}
+		}
 
-        #Check if tar( or tar libraries) are available
-        #tar files 
-        puts "tar -czf #{Configdir}/config_egg.tar.gz #{files}"
-    end
+		#Determine output location
+		if options[:dest] then 
+			location = Files.join(Configdir, "config_egg.tar.gz")
+		else
+			location = "config_egg.tar.gz"
+		end
+
+		#Write tar file
+		puts "tar -czf #{location} #{list.join(' ')}"
+		`tar -czf #{location} #{list.join(' ')}`
+
+		#Until I can figue out how to tar a directory
+		#File.open(location,"wb") { |file|
+			#Zlib::GzipWriter.wrap(file) { |gz|
+				#Gem::Package::TarWriter.new(gz) { |tar|
+					#list.each { |f|
+						#File.open(f,'r') { |fp|
+							#tar.add_file(Pathname.new(f),0444) { |io|
+								#io.write(fp.read)
+							#}
+						#}
+					#}
+				#}
+			#}
+		#}
+	end
 
     desc "alien", "prints an alien"
     def alien()
